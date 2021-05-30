c87da4e0b64e6d7a1bc9489417f3f4fdb3423e6f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-05-30 17:30:06 -0300

    Agr ta fazendo o login direto com o banco de dados, usando a tabela admins

--- backend/rotas/adminBro.js
+++ backend/rotas/adminBro.js
@@ -1,35 +1,42 @@
 const AdminBro = require("admin-bro");
 const AdminBroExpress = require("@admin-bro/express");
-const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminBroMongoose = require("@admin-bro/mongoose");
 
-const mongoose = require ('mongoose');
-const Usuario = require ('../models/usuario')
-const Exercicio = require ('../models/exercicio')
-const Treino = require ('../models/treino')
+const mongoose = require("mongoose");
+const Usuario = require("../models/usuario");
+const Exercicio = require("../models/exercicio");
+const Treino = require("../models/treino");
+const Admin = require("../models/admin");
 
-AdminBro.registerAdapter(AdminBroMongoose)
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const adminBro = new AdminBro({
-  resources: [Usuario, Exercicio, Treino],
+  resources: [Usuario, Exercicio, Treino, Admin],
   rootPath: "/admin",
 });
 
 //por enquanto ta pegando a senha direto daqui
 const ADMIN = {
-  email: process.env.ADMIN_EMAIL || 'admin@admin.com',
-  password: process.env.ADMIN_PASSWORD || 'senha123'
-}
+  email: process.env.ADMIN_EMAIL || "admin@admin.com",
+  password: process.env.ADMIN_PASSWORD || "senha123",
+};
 
-const router = AdminBroExpress.buildAuthenticatedRouter(adminBro,
-  {
-    cookieName: process.env.ADMIN_COOKIE_NAME || 'admin-bro',
-    cookiePassword: process.env.ADMIN_COOKIE_PASS || 'senha-super-secreta',
-    authenticate: async (email, password) => {
-      if (email === ADMIN.email && password === ADMIN.password){
-        return ADMIN
+const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+  cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
+  cookiePassword: process.env.ADMIN_COOKIE_PASS || "senha-super-secreta",
+  authenticate: async (email, password) => {
+    // let admin;
+    let administrador = await Admin.findOne({ email: email });
+    if (administrador) {
+      if (administrador.password === password) {
+        return administrador;
       }
-      return null
     }
-  });
+  },
+});
 
-module.exports = router
+// if (email === ADMIN.email && password === ADMIN.password) {
+//   return ADMIN;
+// }
+
+module.exports = router;
